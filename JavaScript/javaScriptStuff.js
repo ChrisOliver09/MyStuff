ad28b5d82aa1866d5f68f2fbf1ead8c68f189060
ChrisOliver09 <xsscoopx@gmail.com>
2014-08-08 21:06:15 -0500

    adding javascript helpers

--- JavaScript/javaScriptStuff.js
+++ JavaScript/javaScriptStuff.js
@@ -1,1 +1,17 @@
 //This is the start of beautiful Javascript
+
+confirm("Are you ready to play??")// brings up an alert box with ok or cancel (default values)
+
+var age = prompt("What's your age?"); //this will bring up an alert view and store what the user inputs into the variable
+
+console.log("Hay") //log info to terminal 
+
+var foodDemand = function(food) { //Basic function in javascript. all lines inside as well as the end bracket must end with semi-colon
+    console.log("I want to eat" + " " + food);
+};
+
+foodDemand("Pizza"); //calling the above function
+
+var timesTwo = function(number) { //functions can return (just like any other language)
+    return number * 2;
+};
